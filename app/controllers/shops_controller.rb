0d0bfde5057d515e47862a48c116341af0b5bade
Nicolas <nicolas.pennie@gmail.com>
2018-09-22 21:22:26 +0000

    Simplify Shop params and update tests

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -34,7 +34,7 @@ class ShopsController < ApplicationController
 	private
 	
 		def shop_params
-			params.require(:shop).permit(:name)
+			params.permit(:name)
 		end
 		
 		def find_shop
--- test/controllers/shops_controller_test.rb
+++ test/controllers/shops_controller_test.rb
@@ -9,6 +9,34 @@ class ShopsControllerTest < ActionDispatch::IntegrationTest
   	get shops_path
   	assert_response :success
   	assert_not json.empty?
+  	assert_equal Shop.count, json.length
   end
   
+  test "create should add a Shop" do
+  	assert_difference 'Shop.count', 1 do
+  		post shops_path, params: { name: "Valid name" }
+  	end
+  	assert_response :success
+  	assert_not json.empty?
+  	assert_equal "Valid name", json["name"]
+  end
+  
+  test "show should find a shop" do
+  	get shop_path(@shop)
+  	assert_response :success
+  	assert_not json.empty?
+  	assert_equal @shop.name, json["name"]
+  end
+  
+  test "update should update a shop" do
+  	patch shop_path(@shop), params: { name: "New name" }
+  	assert_response :success
+  end
+  
+  test "delete should delete a shop" do
+  	assert_difference 'Shop.count', -1 do
+  		delete shop_path(@shop)
+  	end
+  	assert_response :success
+  end
 end
